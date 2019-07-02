44a969c8325fa6d6ee4f49dc21e17a0afb9cba7a
Jade Huynh <35jadeh@gmail.com>
2019-07-02 16:06:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-var name = "you"
-var holiday = "Independence Day"
-var days = "20"
 
 function happyHolidays()
 {
-  return "Happy holidays!";
+  return "Happy holidays!"
 }
 
 function happyHolidaysTo(name)
 {
-  'return "Happy holidays, ${name}!"'
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name)
 {
-  return "Happy ${holiday}, ${name}!"
+  return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday)
+function holidayCountdown(holiday, days)
 {
-  return 'It\'s ${days} days until ${holiday}!';
+  return `It\'s ${days} days until ${holiday}!`;
 }
